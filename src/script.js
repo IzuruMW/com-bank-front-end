9a3292814dc0b17d0c5fbfddec531ae5c75b4b4a
Isuru <izurumw@gmail.com>
2023-11-23 14:08:20 +0530

    completed landing page

--- src/script.js
+++ src/script.js
@@ -72,24 +72,6 @@ function show7 () {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $(document).ready(function() {
     $(".btn-pref .btn").click(function () {
         $(".btn-pref .btn").removeClass("btn-primary").addClass("text-ro");
@@ -101,6 +83,7 @@ $(document).ready(function() {
         // $(".tab").addClass("active"); // instead of this do the below 
         $(this).removeClass("text-ro").addClass("btntrangle");   
     });
+   
     });
          
 // function show1() {
@@ -109,7 +92,27 @@ $(document).ready(function() {
              
 // function show2() {
 //     document.getElementById('btnbtn').style.color = "white"
-//     }         
+//     }
 //     function show3() {
 //         document.getElementById('btnbtn').style.color = "white"
 //         }
+
+
+// function businessBanking() {
+//     document.getElementById('corpBank').style.display='block';
+// }
+function openCity(evt, comBank) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(comBank).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+document.getElementById("defaultOpen").click();
